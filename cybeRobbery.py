c595082f31b1a6372756a17f18da297a87a2e9e9
atropos148 <lacroix148@gmail.com>
2018-05-19 10:38:29 +0200

    Player class functions
    - created player functions from code in main file

--- cybeRobbery.py
+++ cybeRobbery.py
@@ -4,15 +4,10 @@
 
 from settings import *
 
-from random import randint
-
-
-# import time
-# import os
-# import sys
 
 def setup():
     player = Player(
+        # name will be chosen by player later
         name='Joe',
         money=200,
         heat=0,
@@ -20,7 +15,7 @@ def setup():
         inventory=[],
     )
 
-    world = World(
+    server_farm = ServerFarm(
         server_security=100,
     )
 
@@ -29,7 +24,7 @@ def setup():
         intel_amount=30,
     )
 
-    return [player, world, store]
+    return [player, server_farm, store]
 
 
 def main_menu():
@@ -62,7 +57,7 @@ def main_game(setup_list):
     # TODO: ADD Name Choice
 
     player = setup_list[0]
-    world = setup_list[1]
+    server_farm = setup_list[1]
     store = setup_list[2]
 
     game_loop = True
@@ -72,11 +67,10 @@ def main_game(setup_list):
             print("You attracted too much heat. AugCops kicked down your door. Have fun in prison!")
             game_loop = False
 
-        input("Press Enter")
+        input("Continue...")
 
         print(42 * "-")
         print("- {} credits ----- {} heat ----- {} intel -".format(player.money, player.heat, player.intel))
-        # print("- {} -".format(time.ascitime(time.localtime(time.time()))))
         print("1. Rob a store")
         print("2. Lay low")
         print("3. Gain intel")
@@ -90,98 +84,23 @@ def main_game(setup_list):
 
             # ROB A STORE
             if game_choice == 1:
-                stolen_money = randint(-50, 150)
-                heat_change = randint(1, 10)
-
-                player.heat += heat_change
-                player.money += stolen_money
-
-                print("You attracted {} heat.".format(player.heat))
-                print("You robbed a store...")
-                if stolen_money > 0:
-                    print("You got {} credits.".format(stolen_money))
-                elif stolen_money < 0:
-                    print("You lost {} credits.".format(stolen_money))
-                else:
-                    print("But you got nothing.")
+                player.rob_a_store()
 
             # LAY LOW
             elif game_choice == 2:
-                heat_change = 5
-                if (player.heat - heat_change) <= 0:
-                    player.heat = 0
-                else:
-                    player.heat -= heat_change
-
-                if player.heat > 0:
-                    print("You lay low for a week. You lost some heat")
-
-                else:
-                    print("You chill with some friends for a week.")
+                player.lay_low()
 
             # GAIN INTEL
             elif game_choice == 3:
-                intel_gain = randint(1, 20)
-                heat_change = randint(1, 15)
-
-                player.heat += heat_change
-                player.intel += intel_gain
-
-                print("You attracted {} heat.".format(player.heat))
-                print("Also, you gained {} intel.".format(intel_gain))
+                player.gain_intel()
 
             # ATTACK SERVER FARM
             elif game_choice == 4:
-                world.server_security += (player.heat / 2)
-
-                if (world.server_security - player.intel) <= 50:
-
-                    stolen_money = randint(800, 5000)
-                    heat_change = randint(1, 10)
-
-                    player.heat += heat_change
-                    player.money += stolen_money
-
-                    player.intel = 0
-
-                    print("You got around all security.")
-                    print("The stolen data was sold for {} credits.".format(stolen_money))
-                    print("Also, the corp put out a warrant for you. Gain {} heat.".format(heat_change))
-
-                else:
-
-                    stolen_money = randint(100, 800)
-                    heat_change = randint(10, 25)
-
-                    player.heat += heat_change
-                    player.money += stolen_money
-
-                    player.intel = 0
-
-                    print("You barely got out of there.")
-                    print("Scraps of data you collected were sold on black market for {} credits.".format(stolen_money))
-                    print("Corp is breathing down your neck. Gain {} heat.".format(heat_change))
+                player.attack_server_farm(server_farm)
 
             # STORE
             elif game_choice == 5:
-
-                print("1. Pay {} credits for {} intel".format(store.intel_price, store.intel_amount))
-                print("2. Leave shop")
-
-                try:
-                    game_choice = int(input("Enter your choice [1-2]:"))
-
-                    if game_choice == 1:
-                        player.money -= store.intel_price
-                        player.intel += store.intel_amount
-
-                        print("The girl behind the counter sells you some juicy intel.")
-
-                    elif game_choice == 2:
-                        print("You leave without buying anything.")
-
-                except ValueError:
-                    print("Only use numbers")
+                player.go_shopping(store)
 
             # EXIT
             elif game_choice == 6:
--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Player:
     def __init__(self, name, money, heat, intel, inventory):
         self.name = name
@@ -6,8 +9,100 @@ class Player:
         self.intel = intel
         self.inventory = inventory
 
+    def rob_a_store(self):
+        heat_change = randint(1, 10)
+        stolen_money = randint(0, 150)
 
-class World:
+        self.heat += heat_change
+        self.money += stolen_money
+
+        print("You robbed a store...")
+        print("You attracted {} heat.".format(self.heat))
+
+        if stolen_money > 0:
+            print("You got {} credits.".format(stolen_money))
+        else:
+            print("But you got nothing.")
+
+    def lay_low(self):
+        heat_change = 5
+        if (self.heat - heat_change) <= 0:
+            self.heat = 0
+        else:
+            self.heat -= heat_change
+
+        if self.heat > 0:
+            print("You lay low for a week. You lost some heat.")
+
+        else:
+            print("You chill with some friends for a week.")
+
+    def gain_intel(self):
+        intel_gain = randint(1, 20)
+        heat_change = randint(1, 15)
+
+        self.heat += heat_change
+        self.intel += intel_gain
+
+        print("You attracted {} heat.".format(heat_change))
+        print("Also, you gained {} intel.".format(intel_gain))
+
+    def attack_server_farm(self, server_farm):
+        server_farm.server_security += (self.heat / 2)
+
+        if (server_farm.server_security - self.intel) <= 50:
+
+            stolen_money = randint(800, 5000)
+            heat_change = randint(1, 15)
+
+            self.heat += heat_change
+            self.money += stolen_money
+
+            self.intel = 0
+
+            print("You got around all security.")
+            print("The stolen data was sold for {} credits.".format(stolen_money))
+            print("Also, the corp put out a warrant for you. Gain {} heat.".format(heat_change))
+
+        else:
+
+            stolen_money = randint(100, 800)
+            heat_change = randint(10, 25)
+
+            self.heat += heat_change
+            self.money += stolen_money
+
+            self.intel = 0
+
+            print("You barely got out of there.")
+            print("Scraps of data you collected were sold on black market for {} credits.".format(stolen_money))
+            print("Corp is breathing down your neck. Gain {} heat.".format(heat_change))
+
+    def go_shopping(self, store):
+        print("1. Pay {} credits for {} intel".format(store.intel_price, store.intel_amount))
+        print("2. Leave shop")
+
+        try:
+            game_choice = int(input("Enter your choice [1-2]:"))
+            if self.money < store.intel_price:
+                print("You don't have enough money to buy anything.")
+                game_choice = 2
+
+            if game_choice == 1:
+
+                self.money -= store.intel_price
+                self.intel += store.intel_amount
+
+                print("The girl behind the counter sells you some juicy intel.")
+
+            elif game_choice == 2:
+                print("You leave without buying anything.")
+
+        except ValueError:
+            print("Only use numbers")
+
+
+class ServerFarm:
     def __init__(self, server_security):
         self.server_security = server_security
 
